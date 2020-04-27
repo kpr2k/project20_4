61850042fe6c9fb85d7dead297ce1cff2f0b5c90
meirdon <patirazor@yahoo.pl>
2020-04-27 21:10:07 +0200

    dodałem wykresy

--- src/main/java/gr4/controllers/Controller.java
+++ src/main/java/gr4/controllers/Controller.java
@@ -2,24 +2,31 @@ package gr4.controllers;
 
 import com.opencsv.CSVReader;
 import gr4.dane;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class Controller extends Component implements Initializable {
     @FXML
@@ -30,7 +37,20 @@ public class Controller extends Component implements Initializable {
 
     @FXML
     private Button btnWektor;
+    @FXML
+    private Pane chart;
+    @FXML
+    private TextField kolumnaX;
 
+    @FXML
+    private TextField kolumnaY;
+
+    @FXML
+    private Button ok;
+
+
+    JFrame f;
+    JTable k;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -39,6 +59,7 @@ public class Controller extends Component implements Initializable {
 
     public void onClickEvent(javafx.scene.input.MouseEvent event) throws IOException {
         if(event.getSource()==btnRead) {
+            ArrayList<String> odczyt = new ArrayList();
             String sciezkaDoPlik;
             JFileChooser otworz= new JFileChooser();
             int wynik = otworz.showOpenDialog(this);
@@ -47,58 +68,34 @@ public class Controller extends Component implements Initializable {
                 dane dane = new dane();
                 sciezkaDoPlik= otworz.getSelectedFile().getPath();
                 dane.odczytajPlik(sciezkaDoPlik);
-                for(int i = 0; i<dane.daneOdczytane.length; i++)
-                {
-                    for(int j = 0; j<dane.daneOdczytane[i].length; j++){
-                        System.out.print(dane.daneOdczytane[i][j]+" ");
+                for(int i=0; i<dane.daneOdczytane[i].length;i++) {
+                    if(dane.daneOdczytane[0][i]!=null) {
+                        TableColumn tmp = new TableColumn("Kolumna "+(i+1));
+                        odczyt.add("Kolumna"+(i+1));
                     }
-                    System.out.print("\n");
                 }
-                dane.podzialNaZbiory();
+                f = new JFrame();
+                f.setTitle("Szo to za staroc");
+                String[][] data = dane.daneOdczytane;
+                String[] columnNames = new String[odczyt.size()];
+                for(int i=0;i<odczyt.size();i++) {
+                    columnNames[i]=odczyt.get(i);
+                }
+                k = new JTable(data, columnNames);
+                k.getAutoResizeMode();
+                JScrollPane sp = new JScrollPane(k);
+                f.add(sp);
+                f.setVisible(true);
             }
         } else if (event.getSource()==btnChart) {
-            final NumberAxis yAxis = new NumberAxis(0, 15, 1);
-            final NumberAxis xAxis = new NumberAxis(-2, 20, 1);
+             drawChart(1,2);
 
-            final ScatterChart<Number, Number> lineChart = new ScatterChart<>(xAxis, yAxis);
-            yAxis.setLabel("OSY");
-            xAxis.setLabel("OSX");
-            lineChart.setTitle("Klasy");
-
-            XYChart.Series seriesA = new XYChart.Series();
-            XYChart.Series seriesB = new XYChart.Series();
-            XYChart.Series seriesC = new XYChart.Series();
-
-            seriesA.setName("Klasa A");
-            seriesB.setName("Klasa B");
-            seriesC.setName("Klasa C");
-
-            try (CSVReader dataReader = new CSVReader(new FileReader("C:\\Users\\DELL\\Documents\\przyklad1.csv"))) {
-                String[] nextLine;
-                while ((nextLine = dataReader.readNext()) != null) {
-                    int axisX = Integer.parseInt(nextLine[0]);
-                    int axisY = Integer.parseInt(nextLine[1]);
-                    String klasa = String.valueOf(nextLine[2]);
-                    if (klasa.equals("KlasaA")) {
-                        seriesA.getData().add(new XYChart.Data(axisX, axisY));
-
-                    } else if (klasa.equals("KlasaB")) {
-                        seriesB.getData().add(new XYChart.Data(axisX, axisY));
-                    } else if (klasa.equals("KlasaC")) {
-                        seriesC.getData().add(new XYChart.Data(axisX, axisY));
-                    }
-                }
-
-                lineChart.getData().addAll(seriesA, seriesB, seriesC);
-                Scene scene = new Scene(lineChart, 500, 400);
-                Stage stage = new Stage();
-                stage.setTitle("Wykres");
-                stage.setScene(scene);
-                stage.show();
-            }
-
-
-        } else if (event.getSource()==btnWektor) {
+        }else if (event.getSource()==btnWektor) {
+            /**
+             * Użytkownik musi mieć pole do wpisania:
+             * - liczby p
+             * - liczby k
+             */
             String sciezkaDoPlik;
             JFileChooser otworz= new JFileChooser();
             int wynik = otworz.showOpenDialog(this);
@@ -117,9 +114,91 @@ public class Controller extends Component implements Initializable {
                 }
                 String[] cancer = {"4","1","2","4","2","1","2","1","1"};
                 String[] klasy = {"3","6"};
-                System.out.println(dane.klasyfikujWektor(cancer));
+
+                dane.podzialNaZbiory(150);
+                //System.out.println(dane.klasyfikujWektor(cancer, 2 ,3));
+                System.out.println("h(x) = "+dane.wyznaczDokladnosc(2,3));
             }
         }
-        }
     }
+
+    public void onAction(javafx.event.ActionEvent event) throws  IOException{
+        FXMLLoader loader = new FXMLLoader(new File("src/main/resources/gr4/second.fxml").toURI().toURL());
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/Main.css").toExternalForm());
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    public void drawChart(int kolumnaX, int kolumnaY) {
+        ArrayList<XYChart.Series> seriesArrayList = new ArrayList<>();
+        final NumberAxis yAxis = new NumberAxis();
+        final NumberAxis xAxis = new NumberAxis();
+        final ScatterChart<Number, Number> lineChart = new ScatterChart<>(xAxis, yAxis);
+        yAxis.setLabel("OSY");
+        xAxis.setLabel("OSX");
+        HashSet<String> h = new HashSet<String>();
+        for(int i = 0; i<dane.daneOdczytane.length; i++) {
+            if(dane.daneOdczytane[i][dane.daneOdczytane[i].length-1]!=null &&!(dane.daneOdczytane[i][dane.daneOdczytane[i].length-1].equals("Y"))
+                    && !(dane.daneOdczytane[i][dane.daneOdczytane[i].length-1].equals("default payment next month"))) {
+                h.add(dane.daneOdczytane[i][dane.daneOdczytane[i].length-1]);
+            }
+        }
+        Iterator<String> c = h.iterator();
+        System.out.println(h.toString());
+        while (c.hasNext()) {
+            XYChart.Series tmp = new XYChart.Series();
+            tmp.setName(c.next());
+            seriesArrayList.add(tmp);
+        };
+        if(seriesArrayList.get(0).getName().equals("0")&&seriesArrayList.get(1).getName().equals("1")){
+            System.out.println(dane.daneOdczytane.length);
+            Iterator<XYChart.Series> seriesIterator = seriesArrayList.iterator();
+            System.out.println(seriesArrayList.toString());
+            for(int i = 2; i<dane.daneOdczytane.length; i++)
+            {
+                for (int k=0; k<seriesArrayList.size();k++) {
+                    if ((dane.daneOdczytane[i][dane.daneOdczytane[i].length - 1]).equals(seriesArrayList.get(k).getName())) {
+                        int axisX = Integer.parseInt(dane.daneOdczytane[i][kolumnaX-1]);
+                        int axisY = Integer.parseInt(dane.daneOdczytane[i][kolumnaY-1]);
+                        seriesArrayList.get(k).getData().add(new XYChart.Data(axisX, axisY));
+                    }
+                }
+            }
+            while (seriesIterator.hasNext()) lineChart.getData().addAll(seriesIterator.next());
+            Scene scene = new Scene(lineChart, 800, 600);
+            Stage stage = new Stage();
+            stage.setTitle("Wykres");
+            stage.setScene(scene);
+            stage.show();
+        }else {
+            System.out.println(dane.daneOdczytane.length);
+            Iterator<XYChart.Series> seriesIterator = seriesArrayList.iterator();
+            System.out.println(seriesArrayList.toString());
+            for (int i = 0; i < dane.daneOdczytane.length; i++) {
+                for (int k = 0; k < seriesArrayList.size(); k++) {
+                    if ((dane.daneOdczytane[i][dane.daneOdczytane[i].length - 1]).equals(seriesArrayList.get(k).getName())) {
+                        int axisX = Integer.parseInt(dane.daneOdczytane[i][kolumnaX - 1]);
+                        int axisY = Integer.parseInt(dane.daneOdczytane[i][kolumnaY - 1]);
+                        seriesArrayList.get(k).getData().add(new XYChart.Data(axisX, axisY));
+                    }
+                }
+            }
+            while (seriesIterator.hasNext()) lineChart.getData().addAll(seriesIterator.next());
+            Scene scene = new Scene(lineChart, 500, 400);
+            Stage stage = new Stage();
+            stage.setTitle("Wykres");
+            stage.setScene(scene);
+            stage.show();
+        }
+
+    }
+
+
+}
+
+
+
 
--- src/main/java/gr4/dane.java
+++ src/main/java/gr4/dane.java
@@ -1,6 +1,7 @@
 package gr4;
 
 import org.apache.commons.lang3.math.NumberUtils;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,6 +18,8 @@ public class dane {
     public static String[][] daneOdczytane;
     public String[][] zbior_uczacy;
     public String[][] zbior_testowy;
+    public String[][] tabL;
+
     public void odczytajPlik(String nazwaPliku) {
         // Tworzymy obiekt typu Path
         Path sciezka = Paths.get(nazwaPliku);
@@ -41,7 +44,7 @@ public class dane {
             int[] liniaDouble = new int[liniaDaneString.length];
             // Pętla pobiera z tablicy String liczbe i konwertuje ją na double i zapisuje w tablicy double[]
             for (int i = 0; i < liniaDouble.length; i++) {
-                if(NumberUtils.isParsable(liniaDaneString[i]))
+                if (NumberUtils.isParsable(liniaDaneString[i]))
                     liniaDouble[i] = Integer.parseInt(liniaDaneString[i]);
             }
             // Dodajemy tablicę z serią danych do tablicy z wszystkimi danymi
@@ -51,60 +54,127 @@ public class dane {
 
     }
 
-    public String klasyfikujWektor(String[] w){
+    public String klasyfikujWektor(String[] w, double p, int k) {
+        tabL = new String[k][2]; // tablica najbliższych sąsiadów
         String klasa = "";
-        double L=0;
+        double L = 0;
         double tmp;
-        double p = 2;
-        double iloraz = 1/p;
-        for(int i = 0; i<daneOdczytane.length; i++)
-        {
-            tmp=0;
-            for(int j =0; j<daneOdczytane[i].length;j++){
-                if(NumberUtils.isParsable(daneOdczytane[i][j])) {
-                    tmp += pow(abs((Integer.parseInt(daneOdczytane[i][j])) - Integer.parseInt(w[j])), p);
+        int tmp2 = 0;
+        double max = 0;
+        double iloraz = 1 / p;
+        boolean flag = false;
+        for (int i = 0; i < zbior_uczacy.length; i++) { // zewnętrzena pętla po zbiorze uczących
+            tmp = 0;
+            for (int j = 0; j < zbior_uczacy[i].length; j++) { // wewnętrzna pętla po zbiore uczących
+                if (NumberUtils.isParsable(zbior_uczacy[i][j])) {
+                    tmp += pow(abs((Integer.parseInt(zbior_uczacy[i][j])) - Integer.parseInt(w[j])), p);
+                }
+                if (zbior_uczacy[i][j] instanceof String) {
+                    klasa = zbior_uczacy[i][j];
                 }
             }
-            tmp=pow(tmp,iloraz);
-            System.out.println("i = "+i+", L= "+tmp);
-            if(L==0||tmp<L){
-                L=tmp;
-                for(int k =0; k<daneOdczytane[i].length;k++){
-                    if( daneOdczytane[i][k] instanceof String ) {
-                        klasa = daneOdczytane[i][k];
+            tmp = pow(tmp, iloraz);
+            System.out.println("i = " + i + ", " + klasa + ", L= " + tmp);
+            for (int n = 0; n < tabL.length; n++) { // pętla po tablicy najbliższych sąsiadów
+                if (tabL[n][1] == null) { // wypełnienie tablicy startowymi wartościami
+                    tabL[n][1] = Double.toString(tmp);
+                    tabL[n][0] = klasa;
+                    max = Double.parseDouble(tabL[0][1]);
+                    tmp2 = 0;
+                    break;
+                } else if (i >= tabL.length) {
+                    flag = true;
+                    if (Double.parseDouble(tabL[n][1]) > max) { // wyznaczenie maksymalnej odległości w tablicy najbliższych sąsiadów
+                        max = Double.parseDouble(tabL[n][1]);
+                        tmp2 = n;
                     }
                 }
             }
+            if (Double.parseDouble(tabL[tmp2][1]) > tmp && flag == true) { // zamiana maxa na mniejszą wartość z tablicy uczących
+                tabL[tmp2][1] = Double.toString(tmp);
+                tabL[tmp2][0] = klasa;
+                max = Double.parseDouble(tabL[0][1]);
+                tmp2 = 0;
+            }
         }
-        System.out.print("L= "+L+", ");
+        int[] results = new int[k]; // ilości powtórzeń
+        String[] tabKlasy = new String[tabL.length]; // sąsiadujące klasy
+        for (int i = 0; i < tabL.length; i++) {
+            tabKlasy[i] = tabL[i][0];
+        }
+        Arrays.sort(tabKlasy);
+        int licznik = 1;
+        max = 0;
+        int wierszMaxa = 0;
+        for (int i = 1; i < tabKlasy.length; i++) { // zliczanie powtórzeń
+            if (tabKlasy[i].equals(tabKlasy[i - 1]) == true) {
+                licznik++;
+            } else {
+                results[i - 1] = licznik;
+                licznik = 1;
+                if (results[i - 1] > max) {
+                    max = results[i - 1];
+                    wierszMaxa = i - 1;
+                }
+            }
+        }
+        results[tabKlasy.length - 1] = licznik;
+        if (results[tabKlasy.length - 1] > max) {
+            max = results[tabKlasy.length - 1];
+            wierszMaxa = tabKlasy.length - 1;
+        }
+        klasa = tabKlasy[wierszMaxa]; // wynik końcowy
+
+        System.out.print("\nTablica z najbliższymi sasiadami i ich długość:");
+        for (int i = 0; i < tabL.length; i++) {
+            System.out.println();
+            for (int j = 0; j < tabL[i].length; j++) {
+                System.out.print(tabL[i][j] + ", ");
+            }
+        }
+        System.out.println("\n\nPosortowana tablica z najbliższymi sąsiadami:");
+        for (int i = 0; i < tabKlasy.length; i++) {
+            System.out.print(tabKlasy[i] + ", ");
+        }
+        System.out.println("\n\nTablica z ilością powtórzeń");
+        for (int i = 0; i < results.length; i++) {
+            System.out.print(results[i] + ", ");
+        }
+        System.out.println();
+        System.out.println();
+        for (int i = 0; i < tabKlasy.length; i++) { //Wyświetlanie
+            if (results[i] > 0) {
+                System.out.println(tabKlasy[i] + " - " + results[i] + " (powtórzenia)");
+            }
+        }
         return klasa;
     }
 
-    public void podzialNaZbiory() {
-        String[][] dane1 =  daneOdczytane;
+    public void podzialNaZbiory(int rozmiar_uczacy) {
+        String[][] dane1 = daneOdczytane;
         Integer[] dane2 = new Integer[daneOdczytane.length];
 
-        for(int i=0;i<dane1.length;i++){
+        for (int i = 0; i < dane1.length; i++) {
             dane2[i] = i;
         }
         List<Integer> lista = Arrays.asList(dane2);
 
         Collections.shuffle(lista);
-
-        int ind1 = (lista.size() * 8)/10;
+        int ind1 = rozmiar_uczacy;
+        //int ind1 = (lista.size() * rozmiar_uczacy) / 100;
         String[][] zbior_uczacy = new String[ind1][dane1.length];
-        String[][] zbior_testowy = new String[dane1.length-ind1][dane1.length];
+        String[][] zbior_testowy = new String[dane1.length - ind1][dane1.length];
 
 
         int zm;
-        List<Integer> l1 =  lista.subList(0, ind1);
-        for(int i=0;i<l1.size();i++){
+        List<Integer> l1 = lista.subList(0, ind1);
+        for (int i = 0; i < l1.size(); i++) {
             zm = l1.get(i);
             zbior_uczacy[i] = dane1[zm];
         }
 
         List<Integer> l2 = lista.subList(ind1, lista.size());
-        for(int i=0;i<l2.size();i++){
+        for (int i = 0; i < l2.size(); i++) {
             zm = l2.get(i);
             zbior_testowy[i] = dane1[zm];
         }
@@ -130,8 +200,20 @@ public class dane {
             }
             System.out.println();
         }
-
     }
 
+    public double wyznaczDokladnosc(double p, int k){
+
+        double h_x;
+        int zbior_eq = 0;
+        for(int i = 0; i<zbior_testowy.length; i++){
+            //System.out.println((zbior_testowy[i][zbior_testowy[i].length-1]+" = "+klasyfikujWektor(zbior_testowy[i], p, k)));
+            if(zbior_testowy[i][zbior_testowy[i].length-1].equals(klasyfikujWektor(zbior_testowy[i], p, k))){
+                zbior_eq++;
+            }
+        }
+        h_x = ((double)zbior_eq/zbior_testowy.length);
+        return h_x;
+    }
 
 }
